d8c49734b0586206f3921f769c2c39bbcdea9af8
Haastrup ELijah <haastrupea@gmail.com>
2020-05-16 01:51:52 +0100

    [Refractor] Update the error messages
    update the error messages to abstract away the level of activites going
    to prevent malicious mind to guess email/username that exists in the db

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,35 +1,38 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const User = require('../model/users_singup');
 const localStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 
 module.exports = (passport)=> {
-	passport.use(
-			new localStrategy({usernameField:'email'},(email,password,done)=> {
-				User.findOne({email:email})
-				.then((user)=>{
-					if(!user) {
-						return done(null,false,{message:"that email is not registered"});
-					}
+	passport.use(new localStrategy({usernameField:'email'},(email,password,done)=> {
+				User.findOne({email:email},(err,user)=>{
+					if(err){return done(err,false,{message:"somethin went wrong, please try again later"}); }
+					if(!user){ return done(null,false,{message:"Invalid login details"})}
 
+					
 					//match password
 					bcrypt.compare(password,user.password,(err,isMatch)=>{
-						if(err)  throw err;
+					if(err){return done(err,false,{message:"somethin went wrong, please try again later"}); }
+
 
 						if(isMatch) {							
 							return done(null, user);
 						}else {
 							
-							return done(null, false, {message:"password incorrect"})
+							return done(null, false, {message:"Invalid login details"})
 							
 						}
 					})
-				}).catch((error)=> {
-					throw error
+
 				})
+
+
+				
 			})
 			)
+
+
 		passport.serializeUser(function(user, done) {
 	  done(null, user.id);
 	});
@@ -41,4 +44,3 @@ module.exports = (passport)=> {
 	});
 
 }
-
